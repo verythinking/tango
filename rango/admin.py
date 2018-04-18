b08e6bbe018cce69aaad76454ed1d7e3ab8892ee
verythinking <verythinking@qq.com>
2018-04-18 09:47:46 +0000

    添加模型等

--- rango/admin.py
+++ rango/admin.py
@@ -3,4 +3,13 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from rango.models import Category, Page
+
+
+class PageAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'category', 'url')
+
+
+admin.site.register(Category)
+admin.site.register(Page, PageAdmin)
