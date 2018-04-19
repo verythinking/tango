605e26096988ba70889a162cd089dfcd10d44532
verythinking <verythinking@qq.com>
2018-04-19 08:52:21 +0000

    添加表单

--- rango/views.py
+++ rango/views.py
@@ -2,8 +2,11 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
+from rango.forms import PageForm
 
 
 def category(request, category_name_slug):
@@ -16,10 +19,53 @@ def category(request, category_name_slug):
 
         context_dict['pages'] = pages
         context_dict['category'] = category
+        context_dict['category_name_slug'] = category_name_slug
     except Category.DoesNotExist:
         pass
 
     return render(request, 'rango/category.html', context_dict)
+
+
+def add_category(request):
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print form.errors
+    else:
+        form = CategoryForm()
+
+    return render(request, 'rango/add_category.html', {'form': form})
+
+
+def add_page(request, category_name_slug):
+    try:
+        cat = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        cat = None
+
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if cat:
+                page = form.save(commit=False)
+                page.category = cat
+                page.views = 0
+                page.save()
+                return category(request, category_name_slug)
+        else:
+            print form.errors
+
+    else:
+        form = PageForm()
+
+    context_dict = {'form': form, 'category': cat,
+                    'category_name_slug': category_name_slug}
+    return render(request, 'rango/add_page.html', context_dict)
 
 
 def index(request):
