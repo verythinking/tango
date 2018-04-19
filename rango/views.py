5b7ba2b6ffdbfafcac7d8f258cb587bf147f86fa
verythinking <verythinking@qq.com>
2018-04-19 06:22:04 +0000

    视图以及请求参数

--- rango/views.py
+++ rango/views.py
@@ -2,10 +2,33 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from rango.models import Category
+from rango.models import Page
+
+
+def category(request, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        context_dict['category_name'] = category.name
+
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        pass
+
+    return render(request, 'rango/category.html', context_dict)
 
 
 def index(request):
+    category_list = Category.objects.order_by('-likes')[:5]
     context_dict = {
-        'boldmessage': 'I am bold font from the context'
+        'categories': category_list
     }
     return render(request, 'rango/index.html', context_dict)
+
+
+def about(request):
+    return render(request, 'rango/about.html')
