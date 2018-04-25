91076e81e818541642b896ec70f19ddd02bc1cca
verythinking <verythinking@qq.com>
2018-04-25 06:25:37 +0000

    添加cookie和session

--- rango/views.py
+++ rango/views.py
@@ -6,6 +6,8 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
+from datetime import datetime
+
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 
@@ -71,6 +73,10 @@ def add_page(request, category_name_slug):
 
 def register(request):
 
+    if request.session.test_cookie_worked():
+        print ">>> TEST COOKIE WORKED!"
+        request.session.delete_test_cookie()
+
     registered = False
 
     if request.method == 'POST':
@@ -130,11 +136,42 @@ def user_logout(request):
 
 
 def index(request):
+
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {
-        'categories': category_list
-    }
-    return render(request, 'rango/index.html', context_dict)
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list, 'pages': page_list}
+
+    # visits = int(request.COOKIES.get('visits', '1'))
+    visits = request.session.get('visits')
+    if not visits:
+        visits = 1
+    reset_last_visit_time = False
+
+    last_visit = request.session.get('last_visit')
+    # if 'last_visit' in request.COOKIES:
+    #     last_visit = request.COOKIES.get('last_visit')
+    if last_visit:
+        last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
+
+        if (datetime.now() - last_visit_time).seconds > 0:
+            visits = visits + 1
+            reset_last_visit_time = True
+
+    else:
+        reset_last_visit_time = True
+        # context_dict['visits'] = visits
+        # response = render(request, 'rango/index.html', context_dict)
+
+    if reset_last_visit_time:
+        # response.set_cookie('last_visit', datetime.now())
+        # response.set_cookie('visits', visits)
+        request.session['last_visit'] = str(datetime.now())
+        request.session['visits'] = visits
+
+    context_dict['visits'] = visits
+
+    response = render(request, 'rango/index.html', context_dict)
+    return response
 
 
 def about(request):
